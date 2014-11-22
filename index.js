4b862936558048a4cfa013bbeb4462460a2ece6e
Oleg Pesok <olegpesok@gmail.com>
2014-11-21 17:33:45 -0800

    Adding ability to save emails off to a file
    - Node opens a file 'emails.txt' and saves any submissions there.
    - Minimal checking if email is well formed.

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 require('debug')('gitstar-landing');
+var util = require('util');
+var fs = require('fs');
 
 var PORT = 8081;
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/src'));
 
 app.listen(PORT);
+
+app.post('/submit', function(req, res, next) {
+    if (req.body.email && req.body.email.trim()) {
+        fs.open('emails.txt', 'a', function (err, fd) {
+            if (err) {
+                // We don't want to quit (still want to serve up text)
+                console.log(err.message);
+            }
+
+            fs.write(
+                fd,
+                new Buffer(req.body.email + '\n'),
+                0,
+                req.body.email.length + 1,
+                null,
+                function (err) {
+                    if (err) {
+                        // We don't want to quit (still want to serve up text)
+                        console.log(err.message);
+                    }
+                });
+        });
+    }
+
+    res.status(200).json(req.body);
+    next();
+});
